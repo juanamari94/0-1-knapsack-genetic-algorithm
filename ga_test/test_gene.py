06fa6e939c7402df32a5d5c7e83eb92d5c9b5864
Juan Amari <juan@amari.com.ar>
2018-11-24 13:14:14 +0200

    Added Gene class and tests

---
+++ ga_test/test_gene.py
@@ -0,0 +1,31 @@
+import unittest
+
+from genetic_algorithm.Gene import Gene
+
+
+class GeneTest(unittest.TestCase):
+
+    def test_gene_mutation(self):
+        gene = Gene(1, 2)
+        choice_pool = [Gene(2, 3)]
+        gene.mutate(choice_pool, mutation_chance=1.0)
+        self.assertEquals(gene, choice_pool[0])
+
+    def test_gene_mutation_multiple_choice_pool(self):
+        gene = Gene(1, 2)
+        choice_pool = [Gene(2, 3), Gene(4, 5)]
+        gene.mutate(choice_pool, mutation_chance=1.0)
+        self.assertTrue(gene in choice_pool)
+
+    def test_gene_mutation_with_chance(self):
+        gene = Gene(1, 2)
+        choice_pool = [Gene(2, 3), Gene(4, 5)]
+        gene.mutate(choice_pool, mutation_chance=0.2, mutation_seed=1)
+        self.assertTrue(gene in choice_pool)
+
+    def test_gene_non_mutation(self):
+        gene = Gene(1, 2)
+        expected_gene_after_mutation = Gene(1, 2)
+        choice_pool = [Gene(2, 3), Gene(4, 5)]
+        gene.mutate(choice_pool, mutation_chance=0.2, mutation_seed=2)
+        self.assertEquals(gene, expected_gene_after_mutation)
---
+++ genetic_algorithm/Gene.py
@@ -0,0 +1,31 @@
+"""
+Author: Juan Amari
+
+File for the Gene class.
+"""
+
+import random
+
+DEFAULT_MUTATION_CHANCE = 0.2
+
+
+class Gene:
+
+    def __init__(self, value, weight):
+        self.value = value
+        self.weight = weight
+
+    def mutate(self, choice_pool, mutation_seed=None, mutation_chance=DEFAULT_MUTATION_CHANCE):
+        rng = random.Random()
+        if mutation_seed:
+            rng.seed(mutation_seed)
+
+        mutation_trial = rng.uniform(0, 1)
+
+        if mutation_trial <= mutation_chance:
+            new_gene = rng.choice(choice_pool)
+            self.value = new_gene.value
+            self.weight = new_gene.weight
+
+    def __eq__(self, o: object) -> bool:
+        return isinstance(o, Gene) and self.value == o.value and self.weight == o.weight

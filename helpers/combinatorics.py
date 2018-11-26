5e6c4a0fa002384b9208b990f97dc1d6d2756864
Juan Amari <juan@amari.com.ar>
2018-11-26 21:54:53 +0200

    Removed unused helpers

--- helpers/combinatorics.py
+++
@@ -1,7 +0,0 @@
-from itertools import combinations, chain
-
-
-def powerset(iterable):
-    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
-    s = list(iterable)
-    return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
